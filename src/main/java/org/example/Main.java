d6f4eac00ab5885d2b2d78a42fa9811a7e8ebd86
hornet468 <tomhelington@gmail.com>
2025-02-17 20:55:36 +0000

    new features

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,19 @@
 package org.example;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        SAPIClient client = new SAPIClient();
+        SAPIClient client = new SAPIClient("token", 49494949494L );
         client.fetchPrice();
+
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+
+        scheduler.scheduleAtFixedRate(client::fetchPrice,0, 30, TimeUnit.MINUTES);
     }
 }
--- src/main/java/org/example/SAPIClient.java
+++ src/main/java/org/example/SAPIClient.java
@@ -7,18 +7,53 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class SAPIClient {
+
+    private String lastPrice = "";
+    private String lowestPrice;
+    private final String token;
+    private final long id;
+
+    public SAPIClient(String token, long id) {
+        this.id = id;
+        this.token = token;
+    }
+
     public void fetchPrice() {
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder()
-                .uri(URI.create("https://steamcommunity.com/market/priceoverview/?currency=UAH&appid=730&market_hash_name=AK-47%20%7C%20Jaguar%20%28Battle-Scarred%29"))
+                .uri(URI.create("https://steamcommunity.com/market/priceoverview/?currency=UAH&appid=730&market_hash_name=Sticker%20%7C%20One%20Sting"))
                 .build();
         try {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             JSONObject jsonObject = new JSONObject(response.body());
-            String lowestPrice = jsonObject.getString("lowest_price");
-            System.out.print("lowest price is "+lowestPrice);
+            lowestPrice = jsonObject.getString("lowest_price");
+            if (!lowestPrice.equals(lastPrice) && lowestPrice != null) {
+                lastPrice = lowestPrice;
+                sendMessage();
+            }
+            System.out.print("lowest price is " + lowestPrice);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMessage() {
+        try {
+            String encodedText = URLEncoder.encode("lowest price is " + lowestPrice, StandardCharsets.UTF_8);
+            String url = "https://api.telegram.org/bot" + token + "/sendMessage?chat_id=" + id + "&text=" + encodedText;
+
+            HttpClient client = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .build();
+
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
